df03e56ab5cb75bf982afa7fe11dd12753b947bd
NoamProg <38458454+NoamProg@users.noreply.github.com>
2018-12-09 15:28:22 +0200

    confused

--- dsp_hw2.m
+++ dsp_hw2.m
@@ -4,12 +4,15 @@ clear all; close all; clc;
 sys = zpk([],[-0.6+0.6i , -0.6-0.6i , 0.68+0.62i , 0.68-0.62i], 1);
 figure(1);
 pzmap(sys)
+title('zeros - poles map of original transfer function - none zeros');
 hold on;
 [b,a] = zp2tf([],[-0.6+0.6i , -0.6-0.6i , 0.68+0.62i , 0.68-0.62i], 1);
 
 H = tf(b,a, 0.1, 'variable', 'z^-1');
 figure(2)
-bode(H);
+[mag,~,wout] = bode(H);
+plot(wout, mag(:));
+title('theroteical Bode plot (Amp and Phase) of h1 -none zeros')
 
 noise = randn(1,10000);
 y = filter(b,a,noise);
@@ -40,8 +43,28 @@ est_qual1 = estimation_quality(psd, psd_hat, freq, freq_hat);
 
 %% Section 3
 
-H = tf(b,a, 0.1, 'variable', 'z^-1');
-figure()
-bode(H);
+sys1 = zpk(0.9,[-0.6+0.6i , -0.6-0.6i , 0.68+0.62i , 0.68-0.62i], 1);
+figure(4)
+pzmap(sys1)
+title('Zeros- Poles map of transfer with zero at z=0.9');
+[b2,a2] = zp2tf(0.9,[-0.6+0.6i , -0.6-0.6i , 0.68+0.62i , 0.68-0.62i], 1);
+
+H2 = tf(b,a, 0.1, 'variable', 'z^-1');
+
+figure(5)
+[mag2,~,wout2] = bode(H2);
+plot(wout2, mag2(:));
+title('theroteical Bode plot (Amp and Phase) of h2 - zero at 0.9');
+
+noise = randn(1,10000);
+y2 = filter(b2,a2,noise);
+
+%% Section 4
+
+p = 4;
+[freq, psd, sys] = psd_ar(p, y2, b2);
+figure(6)
+
+
 
 
