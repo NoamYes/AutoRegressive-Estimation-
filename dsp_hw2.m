7b03fd9fb1536ae27a7f25a49e1cb304e34e0ab4
NoamProg <38458454+NoamProg@users.noreply.github.com>
2018-12-09 13:17:44 +0200

    graphs

--- dsp_hw2.m
+++ dsp_hw2.m
@@ -1,3 +1,5 @@
+clear all; close all; clc;
+
 %% Section 1
 sys = zpk([],[-0.6+0.6i , -0.6-0.6i , 0.68+0.62i , 0.68-0.62i], 1);
 figure(1);
@@ -6,8 +8,9 @@ pzmap(sys)
 noise = randn(1,10000);
 y = filter(b,a,noise);
 psd = 20*log(abs(fft(y)).^2);
+[~, freq] = freqz(b,a,length(psd));
 figure(2)
-plot(psd);
+plot(freq,psd);
 hold on;
 title('Power Spectrum Destiny of y');
 xlabel('Frequency[Rad/Sec]');
@@ -18,9 +21,12 @@ p = 4;
 frame = y(1000:1511);
 [a_hat,e,k] = levinson(autocorr(frame),p);
 sys_hat = zpk([],a_hat, 1);
-y_hat = filter(b,a,noise);
+y_hat = filter(b,a_hat,noise);
 psd_hat = 20*log(abs(fft(y_hat)).^2);
-plot(psd_hat);
+[~, freq_hat] = freqz(b,a_hat,length(psd_hat));
+figure(2)
+plot(freq_hat,psd_hat);
 title('Power Spectrum Destiny of y');
 xlabel('Frequency');
 ylabel('Magnitude');
+
